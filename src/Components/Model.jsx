c3812bc5108a7e8c9b16022d69773d0bf67a5128
chaitanyasai <anumulacs@iitbhilai.ac.in>
2023-12-17 09:44:44 +0530

    added three js library

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Model.css';
+import * as THREE from 'three';
+import { PLYLoader } from 'three-ply-loader';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+import ThreeModel from './mesh.jsx';
 const Model = ({ projectId }) => {
     return (
         <div className='modelCard'>
             <h3 className='mb-3'>{projectId}</h3>
             <h3>Model</h3>
-            <div className='model-main mb-5'>
+            <div id='model-main'>
+                <ThreeModel />
             </div>
-            <div className="model-status mb-2 d-flex w-[31px] h-[31px] bg-zinc-300 rounded-full justify-content-between align-items-center">
+            {/* <div className="model-status mb-2 d-flex w-[31px] h-[31px] bg-zinc-300 rounded-full justify-content-between align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 31 31" fill="none">
   <circle cx="15.5" cy="15.5" r="15.5" fill="#D9D9D9"/>
 </svg>
@@ -28,7 +34,7 @@ height: '0.3125rem', background: '#DFD9D9', marginTop:'0.5rem'}}></div>
                 <p>start</p>
                 <p>intmediate</p>
                 <p>final</p>
-            </div>
+            </div> */}
         </div>
     );
 }
